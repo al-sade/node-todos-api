39715c934285eda83c68ebeb22fe6f358cdd85c0
Al Sade <alsade15@gmail.com>
2018-11-17 15:29:39 +0200

    config fix

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
-import config from '../config.js'
+const config = require('../../config.json');
 
 mongoose.Promise = Promise;
-mongoose.connect(`mongodb://${config.db.username}:${config.db.pass}@ds057000.mlab.com:57000/als-todo-app`).then((res) => {
+mongoose.connect(`mongodb://${config.db.username}:${config.db.pass}@ds057000.mlab.com:57000/als-todo-app`, {
+    useNewUrlParser: true
+}).then((res) => {
     console.log('Mongoose connected successfully')
 }, (err) => {
     console.log('Mongoose failed to connect', err);
