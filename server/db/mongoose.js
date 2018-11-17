25619daeb143a6a877c51fb22e5b61af0c01ce0b
Al Sade <alsade15@gmail.com>
2018-11-17 15:36:24 +0200

    config test #3

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 // const config = require('../../config.json');
 
 mongoose.Promise = Promise;
-mongoose.connect(`mongodb://alsade:$Alaba1616@ds057000.mlab.com:57000/als-todo-app`, {
+mongoose.connect(`mongodb://alsade:Alaba1616@ds057000.mlab.com:57000/als-todo-app`, {
     useNewUrlParser: true
 }).then((res) => {
     console.log('Mongoose connected successfully')
