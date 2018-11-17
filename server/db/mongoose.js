5bd94949bcae83856b0edf4e67ac0ef114c5ddd8
Al Sade <alsade15@gmail.com>
2018-11-17 02:21:44 +0200

    GET/POST todos

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.Promise = global.Promise;
+mongoose.Promise = Promise;
 mongoose.connect('mongodb://localhost:27017/TodoApp').then((res) => {
     console.log('Mongoose connected successfully')
 }, (err) => {
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var bodyParse = require('body-parser');
+const express = require('express');
+const bodyParse = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} =require('./db/mongoose');
 var {Todo} =require('./models/todo');
@@ -11,7 +12,6 @@ app.use(bodyParse.json());
 
 app.post('/todos', (req, res) => {
     console.log(req.body);
-
     let todo = new Todo({
         text: req.body.text
     });
@@ -21,6 +21,35 @@ app.post('/todos', (req, res) => {
     }, (err) => {
         res.status(400).send(err);
     })
+});
+
+
+app.get('/todos', (req, res) => {
+    let todos = Todo.find().then((todso) => {
+
+        res.send(todos);
+        return todos;
+    }, err => {
+        res.status(400).send(err);
+    })
+});
+
+app.get('/todos/:id', (req, res) => {
+    let id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send('Not a valid id');
+    }
+
+    Todo.findById(id).then((todo) => {
+        if(!todo) {
+            return res.status(404).send('not todo found')
+        }
+        res.send({todo})
+    }).catch((err) => {
+        return res.status(400).send('400')
+    })
+
 });
 
 app.listen(3000, () => {
