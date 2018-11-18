8656340558e65b62121cb8436184780533062039
Al Sade <alsade15@gmail.com>
2018-11-18 16:51:22 +0200

    minor

--- server/server.js
+++ server/server.js
@@ -52,6 +52,6 @@ app.get('/todos/:id', (req, res) => {
 
 });
 
-app.listen(process.env.port || 3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log(`Server is running on: ${port}`);
 });
