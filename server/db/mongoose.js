cd27e03c4c9384ee5bb66af602f298c079fdb2db
Al Sade <alsade15@gmail.com>
2018-11-19 00:59:50 +0200

    CRUD done

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const config = require('../../config.json');
 
 mongoose.Promise = Promise;
-mongoose.connect(`mongodb://${config.db.username}:${config.db.pass}@ds057000.mlab.com:57000/als-todo-app`, {
+mongoose.connect(`mongodb://localhost:27017/TodoApp`, {
     useNewUrlParser: true
 }).then((res) => {
     console.log('Mongoose connected successfully')
--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const express = require('express');
 const bodyParse = require('body-parser');
 const {ObjectID} = require('mongodb');
@@ -53,6 +54,46 @@ app.get('/todos/:id', (req, res) => {
 
 });
 
+app.delete('/todos/:id', (req, res) => {
+    let id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send('Not a valid id');
+    }
+
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if(!todo){
+            res.status(404).send()
+        }
+        res.status(200).send(todo);
+    }).catch((e) => {
+        res.status(400).send();
+    })
+});
+
+app.patch('/todos/:id', (req, res) => {
+    let id = req.params.id;
+    let body = _.pick(req.body, ['text', 'completed']);
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send('Not a valid id');
+    }
+
+        if(_.isBoolean(body.completed) && body.completed) {
+            body.completedAt = new Date().getTime();
+        } else {
+            body.completed = false;
+            body.completedAt = null;
+        }
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    })
+})
 app.listen(port, () => {
     console.log(`Server is running on: ${port}`);
 });
